5063781b39d6c49780974908a65fc80bffa59fb4
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2022-03-30 09:51:52 +1100

    Code contributors tribute page #21.

---
+++ src/elements/Contributors/Contributors.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import Contributor from "./Contributor";
+import CarouselButton from "../../component/carouselButton/CarouselButton";
+
+const Contributors = () => {
+  const contributors = [
+    {
+      id: 1,
+      name: "Engramar Bollas",
+      nickname: "engramar",
+      quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/assets/images/volunteers/Mentor_Engramar.png",
+      role: "Director of Code.Sydney",
+      linkedIn: "https://www.linkedin.com/in/engramarbollas/",
+    },
+    {
+      id: 2,
+      name: "Qj Stout-Spykers",
+      nickname: "QJ",
+      quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/assets/images/volunteers/Mentor_Qj.png",
+      role: "Team Lead / Manager",
+      linkedIn:
+        "https://www.linkedin.com/in/qj-stout-spykers-704b221a1/?originalSubdomain=au",
+    },
+    {
+      id: 3,
+      name: "Luke Zhao",
+      nickname: "Luukeeeee",
+      quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/assets/images/volunteers/WD_Luke.png",
+      role: "Web Developer",
+      linkedIn:
+        "https://www.linkedin.com/in/luke-zhao-398886214/?originalSubdomain=au",
+    },
+    {
+      id: 4,
+      name: "Paolo Garde",
+      nickname: "rpgarde",
+      quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/assets/images/volunteers/Mentor_Paolo.png",
+      role: "Web Developer",
+      linkedIn: "https://www.linkedin.com/in/paologarde/?originalSubdomain=au",
+    },
+    {
+      id: 5,
+      name: "Corey",
+      nickname: "jabu-web",
+      quote:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/assets/images/logo/big-logo.png",
+      role: "Web Developer",
+      linkedIn: "https://github.com/jabu-web",
+    },
+    {
+      id: 6,
+      name: "Jan Xavier Gomez Virgen",
+      nickname: "jandev",
+      quote:
+        " I like helping other developers and at the same time learning more about technologies.",
+      image: "/assets/images/volunteers/WD_Jan.png",
+      role: "Web Developer",
+      linkedIn: "https://www.linkedin.com/in/janv/",
+    },
+  ];
+  const [index, setIndex] = useState(0);
+  const { id, name, nickname, quote, image, role, linkedIn } =
+    contributors[index];
+
+  const onClickSetIndex = (number) => {
+    setIndex(number);
+  };
+
+  useEffect(() => {
+    let interval = setInterval(() => {
+      if (index === 5) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+    }, 15000);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
+  let buttons = () => {
+    let buttons = [];
+    for (let i = 0; i < contributors.length; i++) {
+      buttons.push(
+        <CarouselButton
+          key={contributors[i].id}
+          index={i}
+          onClickSetIndex={onClickSetIndex}
+        />
+      );
+    }
+    return buttons;
+  };
+
+  return (
+    <div className="contributors">
+      <h2 className="subtitle">Contributors</h2>
+      <section className="carousel-section">
+        <Contributor
+          key={id}
+          name={name}
+          nickname={nickname}
+          quote={quote}
+          image={image}
+          role={role}
+          linkedIn={linkedIn}
+        />
+        <div className="carousel-nav">{buttons()}</div>
+      </section>
+    </div>
+  );
+};
+
+export default Contributors;
