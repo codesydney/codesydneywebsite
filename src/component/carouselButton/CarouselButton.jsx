e3ed272808588acf2409cdd3f6fb20b8f3152376
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2022-03-30 09:51:52 +1100

    Code contributors tribute page #21.

---
+++ src/component/carouselButton/CarouselButton.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const CarouselButton = ({ index, onClickSetIndex }) => {
+  let onClickHandler = (event) => {
+    const { value } = event.target;
+    onClickSetIndex(parseInt(value));
+  };
+  return (
+    <>
+      <label className="custom-radio-button" id="carousel-button">
+        <input
+          onChange={onClickHandler}
+          name="codesydney"
+          type="radio"
+          className="carousel-button"
+          id="carousel-button"
+          value={index}
+        />
+      </label>
+    </>
+  );
+};
+
+export default CarouselButton;
---
+++ src/elements/Contributors/Contributor.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+const Contributor = ({ name, nickname, quote, image, role, linkedIn }) => {
+  return (
+    <>
+      <div className="carousel">
+        <div>
+          <img src={image} alt={`${name} ${role}`} />
+        </div>
+        <div className="details">
+          <div className="quotes"> "{quote}"</div>
+          <div className="author">
+            <span></span>
+            <div>
+              <div className="author-wrapper">
+                <div className="social">
+                  <p className="nickname">{nickname}</p>
+                  <a
+                    href={linkedIn}
+                    className="social"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaLinkedin />
+                  </a>
+                </div>
+                <p className="role">{role}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Contributor;
