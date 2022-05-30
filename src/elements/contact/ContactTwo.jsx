67b18584c6312a7e6dbb8c289324a22292c44081
Ella <rafaela.morato1805@gmail.com>
2022-05-30 19:46:54 +1000

    updating contact us page

--- src/elements/contact/ContactTwo.jsx
+++ src/elements/contact/ContactTwo.jsx
@@ -9,18 +9,17 @@ class ContactTwo extends Component {
                     <div className="row row--35 align-items-start">
                         <div className="col-lg-6 order-2 order-lg-1">
                             <div className="section-title text-left mb--50">
-                                <h2 className="title">Contact Us.</h2>
-                                <p className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto cupiditate aperiam neque.</p>
+                                <p className="description">Fill out the form to get in touch with us.</p>
                             </div>
                             <div className="form-wrapper">
                                 <ContactForm />
                             </div>
                         </div>
-                        <div className="col-lg-6 order-1 order-lg-2">
+                        {/* <div className="col-lg-6 order-1 order-lg-2">
                             <div className="thumbnail mb_md--30 mb_sm--30">
                                 <img src="/assets/images/about/about-6.jpg" alt="trydo"/>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
