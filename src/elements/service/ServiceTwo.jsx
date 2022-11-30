45f003d0ea12d02fb259a634d513aad32fee75a1
Foad Botan <foadbotan@gmail.com>
2022-11-30 20:07:08 +1100

    Refactor: convert ServiceTwo into a function comp

--- src/elements/service/ServiceTwo.jsx
+++ src/elements/service/ServiceTwo.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import { FaTrophy, FaMedal } from "react-icons/fa"
 
 const ServiceList = [
@@ -14,45 +14,41 @@ const ServiceList = [
   },
 ]
 
-class ServiceTwo extends Component {
-  render() {
-    let title = "Awards",
-      description = ""
-    return (
-      <React.Fragment>
-        <div className="row">
-          <div className="col-lg-4 col-12">
-            <div className="section-title mt--30 mt_md--5 mt_mobile--5 mb_mobile--10">
-              <h2 className="title">{title}</h2>
-              <p>{description}</p>
-              <div className="service-btn">
-                <a className="btn-transparent rn-btn-dark" href="/service">
-                  <span className="text"></span>
-                </a>
-              </div>
-            </div>
-          </div>
+export default function ServiceTwo() {
+  let title = "Awards",
+    description = ""
 
-          <div className="col-lg-8 col-12 mt_md--50">
-            <div className="row service-one-wrapper">
-              {ServiceList.map((val, i) => (
-                <div className="col-lg-6 col-md-6 col-sm-6 col-12" key={i}>
-                  <a>
-                    <div className="service service__style--2">
-                      <div className="icon">{val.icon}</div>
-                      <div className="content">
-                        <h3 className="title">{val.title}</h3>
-                        <p>{val.description}</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              ))}
-            </div>
+  return (
+    <div className="row">
+      <div className="col-lg-4 col-12">
+        <div className="section-title mt--30 mt_md--5 mt_mobile--5 mb_mobile--10">
+          <h2 className="title">{title}</h2>
+          <p>{description}</p>
+          <div className="service-btn">
+            <a className="btn-transparent rn-btn-dark" href="/service">
+              <span className="text"></span>
+            </a>
           </div>
         </div>
-      </React.Fragment>
-    )
-  }
+      </div>
+
+      <div className="col-lg-8 col-12 mt_md--50">
+        <div className="row service-one-wrapper">
+          {ServiceList.map((val, i) => (
+            <div className="col-lg-6 col-md-6 col-sm-6 col-12" key={i}>
+              <a>
+                <div className="service service__style--2">
+                  <div className="icon">{val.icon}</div>
+                  <div className="content">
+                    <h3 className="title">{val.title}</h3>
+                    <p>{val.description}</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
-export default ServiceTwo
