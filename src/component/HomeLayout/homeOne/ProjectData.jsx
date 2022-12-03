e1747656cc84c8bbf4c5631719d117092e75cb67
Sagar Dobariya <infodsagar@gmail.com>
2022-12-03 21:47:44 +1100

    Creates ProjectData.jsx & Converts Portfolio.jsx to Function component

---
+++ src/component/HomeLayout/homeOne/ProjectData.jsx
@@ -0,0 +1,139 @@
+const projectData = [
+  {
+    title: "Prisoners Aid (ACT) Website Revamp",
+    description:
+      "Prisoners Aid has been operating as a community organisation in Canberra since 1963. Over the years they have assisted thousands of clients - including prisoners, released prisoners, families of prisoners, and those involved in the court system. The project is to modernise and make Prisoners Aid ACT's website more engaging for potential clients, partners and the general public.",
+    team: ["Engramar Bollas JP", "Brooke Semmler", "Jacin Yan"],
+    image: "paact",
+    sponsorWebsite: "https://www.paact.org.au/",
+  },
+  {
+    title: "Prisoners Aid (ACT) CRM",
+    description:
+      "As part of the Prisoners Aid (ACT) engagement, a niche bespoke CRM was built to manage support records of prisoners and their families who are actively being supported by PAACT.",
+    team: [
+      "Jacin Yan",
+      "Engramar Bollas JP",
+      "Luci Temple",
+      "Godsgift Akari",
+      "Jay Kim",
+    ],
+    image: "paact-crm",
+    sponsorWebsite: "https://www.paact.org.au/",
+  },
+  {
+    title: "Illawarra Multicultural Services Migrant Marketplace App (Migram)",
+    description:
+      "Illawara Multicultural Service (IMS) have been supporting culturally and linguistically diverse communities in the Illawarra since 1980, providing assistance to migrants, humanitarian visa entrants, and refugees. Code.Sydney is building a niche marketplace app to help low-skilled migrants get some livelihood through community engagement.",
+    team: [
+      "Praveen Sequeira",
+      "Sam Sinadinovic",
+      "Lillian Luzinsky",
+      "David Taing",
+      "Engramar Bollas JP",
+      "Hadi Khalili",
+      "Paolo Garde",
+      "Hussam Hattar",
+    ],
+    image: "migram",
+    sponsorWebsite: "https://www.ims.org.au/",
+  },
+  {
+    title: "The City of Parramatta Eisteddfod Society Website Revamp",
+    description:
+      "Established in 1951, the City of Parramatta Eisteddfod Society Inc. is a not-for-profit organisation and an association of volunteers whose principal aim is to provide a safe and encouraging performance arts event where the young people of Greater Western Sydney and beyond can showcase their talents. Code.Sydney is chosen to revamp their existing website.",
+    team: [
+      "Stephen Corral",
+      "Hadi Khalili",
+      "Ella Ferreira",
+      "Kevin Pan",
+      "Engramar Bollas JP",
+    ],
+    image: "eisteddfod",
+    sponsorWebsite: "https://www.eisteddfodparramatta.org.au/",
+  },
+  {
+    title: "Talking Time With Lukas and Alicia Podcast Website",
+    description:
+      "Talking Time is a place where uncomfortable conversations about incarceration and everything to do with it take place. It is where the voice of those who have lived it, have loved ones that have or are living it, work in it or have an interest in it, come together in a safe and supportive place.",
+    team: ["Engramar Bollas JP"],
+    image: "talkingtime",
+    sponsorWebsite: "https://talkingtimepodcast.com.au/",
+  },
+  {
+    title: "Lloyd Consulting Co",
+    description:
+      "Lloyd Consulting is the brainchild of Dean M Lloyd. Dean is a person with lived experience in the criminal justice system who acts as a conduit between the world of business and the criminal justice system. In essence, a change agent who teaches companies and organisations how to unlock the value of employing people with a criminal record. Providing the language and vernacular to sustain change.",
+    team: [
+      "Livia Resende ",
+      "Kathryn Kelly ",
+      "Naheen Habib ",
+      "Paul Kang ",
+      "David Teang ",
+      "Engramar Bollas JP",
+    ],
+    image: "lloydco",
+    sponsorWebsite: "https://lloydconsulting.co/",
+  },
+  {
+    title:
+      "Rental Property Extractor App (In aid of searching temporary shelter for the homeless)",
+    description:
+      "An app to help two Sydney-based charity organisations by providing their back office staff a tool to easily extract potential rental properties as temporary accommodation for those who are on the brink of experiencing homelessness.",
+    team: ["Kevin Foong"],
+    image: "propertyextractor",
+    sponsorWebsite: "https://www.propertyextractor.com.au/",
+  },
+  {
+    title: "Women of Colour Australia Mentorship Portal",
+    description:
+      "Women of Colour Australia is a non-profit organisation run by women of colour for women of colour. They exist to champion Australia's Women of Colour through programs of education, community support initiatives, and advocacy work. This project is an online membership portal for Women of Colour Australia which includes event management, chat rooms, online course offerings, on-demand mentorship among others.",
+    team: [
+      "Bao Hoang",
+      "Ksenija Lunic",
+      "Pablo Gayoso",
+      "Max Du",
+      "Engramar Bollas JP",
+    ],
+    image: "woc",
+    sponsorWebsite: "https://womenofcolour.org.au/",
+  },
+  {
+    title: "Dr. Lukas Carey, Advocacy on Convict Criminology Website",
+    description:
+      "Dr. Lukas Carey is a strong advocate for the importance of Convict Criminology and the lived experience to govern the direction of related policies and practices. Code.Sydney is helping Dr. Lukas Carey to boost his online web presence so he can assist more people and organisations alike.",
+    team: ["Stephen Corral"],
+    image: "lukascarey",
+    sponsorWebsite: "https://www.lukascarey.com.au/",
+  },
+  {
+    title: "TasksTech App",
+    description:
+      "An app which helps electricians manage their customers, inventories and work diaries. TasksTech is a bespoke app for a niche group of electricians.",
+    team: [
+      "Kevin Foong",
+      "Stephen Corral",
+      "Engramar Bollas JP",
+      "Fumihito Takanami",
+      "Godsgift Akari",
+      "Max Du",
+      "Chintan Patel",
+      "Gabriel Djan",
+      "Promie Yutasane",
+      "Prashanga Dhakal",
+    ],
+    image: "ttech",
+    sponsorWebsite: "https://www.taskstech.com/",
+  },
+  {
+    title:
+      "Deadly Connections Community and Justice Services Limited Legacy Website",
+    description:
+      "Deadly Connections is a Aboriginal Community Led, Not For Profit Organisation that breaks the cycles of disadvantage and trauma to directly address the over-representation of Aboriginal people in the child protection and justice system/s. Code.Sydney developed the very first website of Deadly Connections.",
+    team: ["Engramar Bollas JP"],
+    image: "dc",
+    sponsorWebsite: "https://deadlyconnections.org.au/",
+  },
+]
+
+export default projectData
