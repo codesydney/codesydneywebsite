ea5f73d3d86e5da01f3c8699a6eef690a4fd08c8
NimaSakha <103934713+NimaSakha@users.noreply.github.com>
2022-12-31 23:30:47 +1100

    removing contributors array
    removed contributors array out of Contributors component

--- src/elements/Contributors/Contributors.jsx
+++ src/elements/Contributors/Contributors.jsx
@@ -2,72 +2,73 @@ import React, { useState, useEffect } from "react"
 import Contributor from "./Contributor"
 import CarouselButton from "../../component/carouselButton/CarouselButton"
 
+const contributors = [
+  {
+    id: 1,
+    name: "Engramar Bollas",
+    nickname: "engramar",
+    image: "/assets/images/volunteers/Mentor_Engramar.png",
+    role: "Director of Code.Sydney",
+    linkedIn: "https://www.linkedin.com/in/engramarbollas/",
+  },
+  {
+    id: 2,
+    name: "Qj Stout-Spykers",
+    nickname: "QJ",
+    image: "/assets/images/volunteers/Mentor_Qj.png",
+    role: "Team Lead / Manager",
+    linkedIn:
+      "https://www.linkedin.com/in/qj-stout-spykers-704b221a1/?originalSubdomain=au",
+  },
+  {
+    id: 3,
+    name: "Luke Zhao",
+    nickname: "Luukeeeee",
+    image: "/assets/images/volunteers/WD_Luke.png",
+    role: "Web Developer",
+    linkedIn:
+      "https://www.linkedin.com/in/luke-zhao-398886214/?originalSubdomain=au",
+  },
+  {
+    id: 4,
+    name: "Paolo Garde",
+    nickname: "rpgarde",
+    image: "/assets/images/volunteers/Mentor_Paolo.png",
+    role: "Web Developer",
+    linkedIn: "https://www.linkedin.com/in/paologarde/?originalSubdomain=au",
+  },
+  {
+    id: 5,
+    name: "Corey",
+    nickname: "jabu-web",
+    image: "/assets/images/logo/big-logo.png",
+    role: "Web Developer",
+    linkedIn: "https://github.com/jabu-web",
+  },
+  {
+    id: 6,
+    name: "Jan Xavier Gomez Virgen",
+    nickname: "jandev",
+    quote:
+      " I like helping other developers and at the same time learning more about technologies.",
+    image: "/assets/images/volunteers/WD_Jan.png",
+    role: "Web Developer",
+    linkedIn: "https://www.linkedin.com/in/janv/",
+  },
+  {
+    id: 7,
+    name: "Ella Ferreira",
+    nickname: "Ella",
+    quote:
+      " It's been a great way of putting my skills in practice and learning with the Mentors as well.",
+    image: "/assets/images/volunteers/WD_Ella.png",
+    role: "Web Developer",
+    linkedIn:
+      "https://www.linkedin.com/in/ella-ferreira-3959aa82/?originalSubdomain=au",
+  },
+]
 const Contributors = () => {
-  const contributors = [
-    {
-      id: 1,
-      name: "Engramar Bollas",
-      nickname: "engramar",
-      image: "/assets/images/volunteers/Mentor_Engramar.png",
-      role: "Director of Code.Sydney",
-      linkedIn: "https://www.linkedin.com/in/engramarbollas/",
-    },
-    {
-      id: 2,
-      name: "Qj Stout-Spykers",
-      nickname: "QJ",
-      image: "/assets/images/volunteers/Mentor_Qj.png",
-      role: "Team Lead / Manager",
-      linkedIn:
-        "https://www.linkedin.com/in/qj-stout-spykers-704b221a1/?originalSubdomain=au",
-    },
-    {
-      id: 3,
-      name: "Luke Zhao",
-      nickname: "Luukeeeee",
-      image: "/assets/images/volunteers/WD_Luke.png",
-      role: "Web Developer",
-      linkedIn:
-        "https://www.linkedin.com/in/luke-zhao-398886214/?originalSubdomain=au",
-    },
-    {
-      id: 4,
-      name: "Paolo Garde",
-      nickname: "rpgarde",
-      image: "/assets/images/volunteers/Mentor_Paolo.png",
-      role: "Web Developer",
-      linkedIn: "https://www.linkedin.com/in/paologarde/?originalSubdomain=au",
-    },
-    {
-      id: 5,
-      name: "Corey",
-      nickname: "jabu-web",
-      image: "/assets/images/logo/big-logo.png",
-      role: "Web Developer",
-      linkedIn: "https://github.com/jabu-web",
-    },
-    {
-      id: 6,
-      name: "Jan Xavier Gomez Virgen",
-      nickname: "jandev",
-      quote:
-        " I like helping other developers and at the same time learning more about technologies.",
-      image: "/assets/images/volunteers/WD_Jan.png",
-      role: "Web Developer",
-      linkedIn: "https://www.linkedin.com/in/janv/",
-    },
-    {
-      id: 7,
-      name: "Ella Ferreira",
-      nickname: "Ella",
-      quote:
-        " It's been a great way of putting my skills in practice and learning with the Mentors as well.",
-      image: "/assets/images/volunteers/WD_Ella.png",
-      role: "Web Developer",
-      linkedIn:
-        "https://www.linkedin.com/in/ella-ferreira-3959aa82/?originalSubdomain=au",
-    },
-  ]
+  
   const [index, setIndex] = useState(0)
   const { id, name, nickname, quote, image, role, linkedIn } =
     contributors[index]
