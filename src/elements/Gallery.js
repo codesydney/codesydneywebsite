0bfb97cb946422d50939fd21f5fe800596e102c8
Engramar Bollas <engramar.bollas@gmail.com>
2024-06-09 13:08:17 +1000

    Added Strava

--- src/elements/Gallery.js
+++ src/elements/Gallery.js
@@ -7,13 +7,34 @@ const Gallery = () => {
   return (
     <>
       <div className="gallery-page">
-        <SubPageTitle title="Gallery" />
+        <SubPageTitle title="Activities" />
         <ImageGallery
           items={database.gallery}
           thumbnailPosition="right"
           autoPlay="true"
           slideInterval={5000}
         />
+      </div>
+
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 'calc(100vh - 100px)', // Adjust this value if you have other content on the page
+        marginTop: '5px' // Optional, adjust as needed
+      }}>
+        <h2>Strava</h2>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <iframe
+            frameBorder='0'
+            height='454'
+            scrolling='no'
+            src='https://www.strava.com/clubs/1099484/latest-rides/b0536fd9883e7a1ad157a13212b5944a1be23882?show_rides=true'
+            width='300'
+            style={{ maxWidth: '100%' }}
+          ></iframe>
+        </div>
       </div>
     </>
   )
